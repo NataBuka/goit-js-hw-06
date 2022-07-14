f479a9c41eb2a88c13d8744c1c2f9c9224097829
NataBuka <natabuksha30@gmail.com>
2022-07-14 15:08:51 -0400

    add task 8, add task 9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", onLoginForm);
+
+function onLoginForm(event){
+    event.preventDefault();
+    const elements = event.currentTarget.elements;
+    const email = elements.email.value;
+    const password = elements.password.value;
+
+    const formData = {
+        email: email,
+        password: password
+    }
+    console.log(formData);
+    if (email === "" || password === "") return alert("Заповніть усі поля");
+    form.reset();
+}
 
